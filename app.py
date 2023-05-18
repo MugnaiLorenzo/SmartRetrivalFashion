555d15bdd6247145cc95b1f5316f5b8a64414247
MugnaiLorenzo <lorenzo.mugnai5@stud.unifi.it>
2023-05-18 21:25:37 +0200

    Add a dataset

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask, render_template
-from data_utilis import server_base_path, dataset_root
+import random
+from typing import Optional
+
+import data_utilis
+import numpy as np
+import PIL.Image
+
+from flask import Flask, render_template, send_file
+from io import BytesIO
+from data_utilis import *
+from data_utilis import targetpad_resize
 
 app = Flask(__name__)
 
 
+@app.before_request
+def _load_assets():
+    data_utilis.read_cvs()
+
+
 @app.route('/')
 def start():  # put application's code here
-    return render_template('result.html')
+    random_indexes = random.sample(range(len(data_utilis.image_id)), k=15)
+    names = np.array(data_utilis.image_id)[random_indexes].tolist()
+    return render_template('result.html', names=names)
+
+
+@app.route('/get_image/<string:image_name>')
+@app.route('/get_image/<string:image_name>/<int:dim>')
+def get_image(image_name: str, dim: Optional[int] = None):
+    name = image_name + ".jpg"
+    image_path = image_root / name
+    if dim:
+        transform = targetpad_resize(1.25, int(dim), 255)
+        pil_image = transform(PIL.Image.open(image_path))
+    else:
+        pil_image = PIL.Image.open(image_path)
+
+    img_io = BytesIO()
+    pil_image.save(img_io, 'JPEG', quality=80)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+
+@app.route('/char_image/<string:image_name>')
+def char_image(image_name: str):
+    char = data_utilis.get_char_image(image_name)
+    print(char['gender'] , char['articleType'], char['baseColour'], char['year'], char['productDisplayName'])
+    gender = char['gender']
+    articleType = char['articleType']
+    baseColour = char['baseColour']
+    year = char['year']
+    productDisplayName = char['productDisplayName']
+    return render_template('feature.html', name=image_name, gender=gender, articleType=articleType, baseColour=baseColour, year=year, productDisplayName=productDisplayName)
 
 
 if __name__ == '__main__':
--- data_utilis.py
+++ data_utilis.py
@@ -1,9 +1,84 @@
+import csv
 from pathlib import Path
+from typing import Optional
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+import torchvision.transforms.functional as F
+import PIL.Image
 
 server_base_path = Path(__file__).absolute().parent.absolute()
-dataset_root = Path(__file__).absolute().parent.absolute() / 'dataset'
+dataset_root = Path(__file__).absolute().parent.absolute() / 'data_set' / 'fashion-dataset'
+image_root = dataset_root / 'images'
+image_id = []
 
 
 # check is an image
 def is_image_file(filename: str) -> bool:
     return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
+
+
+# conversion image to rgb
+def _convert_image_to_rgb(image):
+    return image.convert("RGB")
+
+
+# read cvs file
+def read_cvs():
+    with open(dataset_root / 'styles.csv') as file:
+        csv_reader = csv.DictReader(file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count != 0:
+                image_id.append(row['id'])
+            line_count += 1
+
+
+def get_char_image(name: str):
+    with open(dataset_root / 'styles.csv') as file:
+        csv_reader = csv.DictReader(file, delimiter=',')
+        for row in csv_reader:
+            if row['id'] == name:
+                return row
+    return None
+
+
+class TargetPad:
+    """
+    Pad the image if its aspect ratio is above a target ratio.
+    Pad it to match such target ratio
+    """
+
+    def __init__(self, target_ratio: float, size: int, pad_value: Optional[int] = 0):
+        """
+        :param target_ratio: target ratio
+        :param size: preprocessing output dimension
+        :param pad_value: padding value, 0 is black-pad (zero-pad), 255 is white-pad
+        """
+        self.size = size
+        self.target_ratio = target_ratio
+        self.pad_value = pad_value
+
+    def __call__(self, image):
+        w, h = image.size
+        actual_ratio = max(w, h) / min(w, h)
+        if actual_ratio < self.target_ratio:  # check if the ratio is above or below the target ratio
+            return image
+        scaled_max_wh = max(w, h) / self.target_ratio  # rescale the pad to match the target ratio
+        hp = max(int((scaled_max_wh - w) / 2), 0)
+        vp = max(int((scaled_max_wh - h) / 2), 0)
+        padding = [hp, vp, hp, vp]
+        return F.pad(image, padding, self.pad_value, 'constant')
+
+
+def targetpad_resize(target_ratio: float, dim: int, pad_value: int):
+    """
+    Yield a torchvision transform which resize and center crop an image using TargetPad pad
+    :param target_ratio: target ratio for TargetPad
+    :param dim: image output dimension
+    :param pad_value: padding value, 0 is black-pad (zero-pad), 255 is white-pad
+    :return: torchvision Compose transform
+    """
+    return Compose([
+        TargetPad(target_ratio, dim, pad_value),
+        Resize(dim, interpolation=PIL.Image.BICUBIC),
+        CenterCrop(dim),
+    ])
