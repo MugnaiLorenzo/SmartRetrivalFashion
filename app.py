9d6e8de66142d496b7ad1dc18faeab6ce32ea03f
MugnaiLorenzo <lorenzo.mugnai5@stud.unifi.it>
2023-06-27 15:26:20 +0200

    Clean Code and add docker

--- app.py
+++ app.py
@@ -271,4 +271,6 @@ def delete_image(col, image):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run()
